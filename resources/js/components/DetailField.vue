0d122738cbbbe9717439c2b6cf4566cff474b3e4
Jesper Kold-Hansen <jkoldhansen@gmail.com>
2019-01-28 23:57:27 +0100

    Just an update to the detail field.

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -9,5 +9,10 @@
 <script>
     export default {
         props: ['resource', 'resourceName', 'resourceId', 'field'],
+
+        beforeMount() {
+            this.field.value = JSON.stringify(this.field.value, null, 2)
+            this.field.options = {'mode': 'application/json'}
+        }
     };
 </script>
